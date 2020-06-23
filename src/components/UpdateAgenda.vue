befd29dc7a8c14937c17e5bf6f79e587462fee71
Brami01 <bramopit@gmail.com>
2020-06-23 02:50:38 -0400

    Agenda CRUD

---
+++ src/components/UpdateAgenda.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-dialog v-model="show" persistent max-width="500px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Update agenda</span>
+      </v-card-title>
+      <v-col cols="12">
+        <v-text-field v-model="name" label="Name" required></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="description"
+          label="Description"
+          required
+        ></v-text-field>
+      </v-col>
+      <v-row justify="center">
+        <v-time-picker
+          v-if="this.appointments.length == 0"
+          v-model="start"
+        ></v-time-picker>
+      </v-row>
+      <v-row justify="center">
+        <v-time-picker
+          v-if="this.appointments.length == 0"
+          v-model="end"
+        ></v-time-picker>
+      </v-row>
+      <v-spacer></v-spacer>
+      <v-card-actions>
+        <v-btn color="blue darken-1" text @click.stop="show = false"
+          >Cancel</v-btn
+        >
+        <v-btn
+          color="blue darken-1"
+          text
+          @click.stop="updaagenda(), (show = false)"
+          >Update</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "updateAgenda",
+  props: {
+    value: Boolean
+  },
+  data() {
+    return {
+      agenda: "",
+      name: "",
+      description: "",
+      start: "",
+      end: "",
+      appointments: ""
+    };
+  },
+  methods: {
+    updaagenda() {
+      this.$emit("updaagenda", {
+        agenda: {
+          name: this.name,
+          description: this.description,
+          start: this.start,
+          end: this.end,
+          appointments: this.appointments
+        },
+        name: this.agenda.name
+      });
+      this.agenda = null;
+    },
+    closeDialog() {
+      this.$emit("close-update");
+    },
+    setAgenda(agenda) {
+      this.agenda = agenda;
+      this.name = agenda.name;
+      this.description = agenda.description;
+      this.start = agenda.start;
+      this.end = agenda.end;
+      this.appointments = agenda.appointments;
+    }
+  },
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,67 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    agendas: [
+      {
+        name: "Work",
+        description: "my agenda for work",
+        start: "8:30",
+        end: "18:30",
+        appointments: ["not-null"]
+      },
+      {
+        name: "Personal",
+        description: "my agenda for my things",
+        start: "18:30",
+        end: "21:30",
+        appointments: []
+      },
+      {
+        name: "Entertainment",
+        description: "for entertainment",
+        start: "11:30",
+        end: "14:30",
+        appointments: []
+      }
+    ]
+  },
+  actions: {
+    createAgenda({ commit }, agendaCreated) {
+      commit("mutateCreateAgenda", agendaCreated);
+    },
+    updateAgenda({ commit }, agendaUpdated) {
+      commit("mutateUpdateAgenda", agendaUpdated);
+    },
+    deleteAgenda({ commit }, agendaDeleted) {
+      commit("mutateDeleteAgenda", agendaDeleted);
+    }
+  },
+  mutations: {
+    mutateCreateAgenda(state, agenda) {
+      const foudAgendatIndex = state.agendas.findIndex(
+        agendaFind => agendaFind.name === agenda.name
+      );
+      if (state.agendas[foudAgendatIndex] == null) {
+        state.agendas.push(agenda);
+      } else {
+        alert("Yout can't repeate a name");
+      }
+    },
+    mutateUpdateAgenda(state, agendaName) {
+      const foudAgendaIndex = state.agendas.findIndex(
+        agenda => agenda.name === agendaName.name
+      );
+      state.agendas[foudAgendaIndex] = agendaName.agenda;
+    },
+    mutateDeleteAgenda(state, name) {
+      state.agendas = state.agendas.filter(agenda => agenda.name !== name);
+    }
+  },
+  getters: {
+    getAgendas(state) {
+      return state.agendas;
+    }
+  },
   modules: {}
 });
