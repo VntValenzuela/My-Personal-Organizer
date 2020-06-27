909450c4bcc85172f4b9df469126fce18ffc53fa
PedroBranez <pedrobran8@gmail.com>
2020-06-27 18:08:49 -0400

    Adding filter functionality and some interface changes

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,115 +1,148 @@
 <template>
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar flat color="white">
-          <v-btn
-            color="primary"
-            class="mr-4"
-            dark
-            @click="sendData(selectedAppointment, true)"
-          >
-            New Event
+  <div>
+    <v-app-bar app flat color="white" clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-btn
+        color="primary"
+        class="mr-4"
+        dark
+        @click="sendData(selectedAppointment, true)"
+      >
+        New Event
+      </v-btn>
+      <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+        Today
+      </v-btn>
+      <v-btn fab text small color="grey darken-2" @click="prev">
+        <v-icon small>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn fab text small color="grey darken-2" @click="next" class="mr-4">
+        <v-icon small>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title v-if="$refs.calendar">
+        {{ $refs.calendar.title }}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+            <span>{{ typeToLabel[type] }}</span>
+            <v-icon right>mdi-menu-down</v-icon>
           </v-btn>
-          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
-            Today
-          </v-btn>
-          <v-btn fab text small color="grey darken-2" @click="prev">
-            <v-icon small>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="next"
-            class="mr-4"
-          >
-            <v-icon small>mdi-chevron-right</v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu bottom right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>mdi-menu-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
+        </template>
+        <v-list>
+          <v-list-item @click="type = 'day'">
+            <v-list-item-title>Day</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="type = 'week'">
+            <v-list-item-title>Week</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="type = 'month'">
+            <v-list-item-title>Month</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="type = '4day'">
+            <v-list-item-title>4 days</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-list flat>
+        <v-subheader>Agendas</v-subheader>
+        <v-list-item-group v-model="selectedAgendas" multiple color="indigo">
+          <v-list-item v-for="agenda in agendas" :key="agenda.id">
+            <template v-slot:default="{ active }">
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="active"
+                  :color="agenda.color"
+                ></v-checkbox>
+              </v-list-item-action>
 
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
-          @change="updateRange"
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          offset-x
-        >
-          <v-card color="grey lighten-4" min-width="350px" flat>
-            <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn @click="deleteScheduledAppointment(selectedEvent.id)" icon>
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-            </v-toolbar>
-            <v-card-text>
-              <p>{{ selectedEvent.description }}</p>
-              <p>{{ selectedEvent.startHour }} - {{ selectedEvent.endHour }}</p>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn text color="secondary" @click="selectedOpen = false">
-                Close
-              </v-btn>
-              <v-btn
-                text
-                color="secondary"
-                @click="sendData(selectedEvent, false)"
+              <v-list-item-content>
+                <v-list-item-title>{{ agenda.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{
+                  agenda.description
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <v-sheet height="600">
+              <v-calendar
+                ref="calendar"
+                v-model="focus"
+                color="primary"
+                :events="events"
+                :event-color="getEventColor"
+                :type="type"
+                @click:event="showEvent"
+                @click:more="viewDay"
+                @click:date="viewDay"
+                @change="updateRange"
+              ></v-calendar>
+              <v-menu
+                v-model="selectedOpen"
+                :close-on-content-click="false"
+                :activator="selectedElement"
+                offset-x
               >
-                Edit
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-    <Appointment
-      :selectedAppointment="selectedAppointment"
-      :dialog="dialog"
-      :newAppointment="newAppointment"
-      @close="dialog = false"
-    />
-  </v-row>
+                <v-card color="grey lighten-4" min-width="350px" flat>
+                  <v-toolbar :color="selectedEvent.color" dark>
+                    <v-toolbar-title
+                      v-html="selectedEvent.name"
+                    ></v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                      <v-icon>mdi-calendar-clock</v-icon>
+                    </v-btn>
+                    <v-btn
+                      @click="deleteScheduledAppointment(selectedEvent.id)"
+                      icon
+                    >
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                  </v-toolbar>
+                  <v-card-text>
+                    <p>{{ selectedEvent.description }}</p>
+                    <p>
+                      {{ selectedEvent.startHour }} -
+                      {{ selectedEvent.endHour }}
+                    </p>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text color="secondary" @click="selectedOpen = false">
+                      Close
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="secondary"
+                      @click="sendData(selectedEvent, false)"
+                    >
+                      Edit
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-menu>
+            </v-sheet>
+          </v-col>
+          <Appointment
+            :selectedAppointment="selectedAppointment"
+            :dialog="dialog"
+            :newAppointment="newAppointment"
+            @close="dialog = false"
+          />
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
 </template>
 
 <script>
@@ -138,8 +171,11 @@ export default {
     selectedElement: null,
     selectedOpen: false,
     dialog: false,
+    drawer: false,
     newAppointment: false,
-    selectedAppointment: {}
+    selectedAppointment: {},
+    selectedAgendas: [],
+    filteredEvents: []
     /*events: [
       {
         name: "Cita con el doctor",
@@ -167,19 +203,24 @@ export default {
       return this.getAgendas;
     },
     events() {
-      //console.log("CALENDAR -> events Changed");
-      return this.getScheduledAppointments.map(appointment => {
-        return {
-          ...appointment,
-          start: `${appointment.date} ${appointment.startHour}`,
-          end: `${appointment.date} ${appointment.endHour}`,
-          color: "#ff8080"
-        };
-      });
+      console.log("CALENDAR -> events Changed" + this.filteredEvents);
+      let eventsToShow = [];
+      if (this.filteredEvents) {
+        console.log(`CALENDAR -> L:${this.filteredEvents.length}`);
+        eventsToShow = this.filteredEvents.map(appointment => {
+          return {
+            ...appointment,
+            start: `${appointment.date} ${appointment.startHour}`,
+            end: `${appointment.date} ${appointment.endHour}`
+          };
+        });
+      }
+      return eventsToShow;
     }
   },
   mounted() {
     this.getEvents();
+    this.initSelectedAgendas();
   },
   methods: {
     getEvents() {}, //Falta implementar
@@ -251,14 +292,33 @@ export default {
       }
       this.newAppointment = newAppointment;
       this.dialog = true;
+    },
+    filterEvents() {
+      let eventsFiltered = [];
+      if (this.selectedAgendas.length > 0) {
+        this.selectedAgendas.forEach(index => {
+          const foundAgenda = this.agendas[index];
+          eventsFiltered = eventsFiltered.concat(
+            this.appointments
+              .filter(
+                appointmentF => appointmentF.agendaId === foundAgenda.agendaId
+              )
+              .map(appointment => {
+                return { ...appointment, color: foundAgenda.color };
+              })
+          );
+        });
+      }
+      this.filteredEvents = eventsFiltered;
+    },
+    initSelectedAgendas() {
+      const numberOfAgendas = this.getAgendas.length;
+      this.selectedAgendas = Array.from(Array(numberOfAgendas).keys());
     }
   },
   watch: {
-    appointments: {
-      deep: true,
-      handler() {
-        console.log("CALENDAR -> events Changed");
-      }
+    selectedAgendas() {
+      this.filterEvents();
     }
   }
 };
